c9f9716988deb5e997ed1537dc6ca9b67c5a4e13
minte9 <catalinprescure@gmail.com>
2021-10-18 17:19:59 +0300

    java-pages update

---
+++ src/main/java/com/minte9/basics/loops/EnhancedFor.java
@@ -0,0 +1,12 @@
+package com.minte9.basics.loops;
+
+public class EnhancedFor {            
+    public static void main(String[] args) {
+        
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int no : numbers) {
+            System.out.println(no); // 1 2 .. 5
+        }
+    }
+}
