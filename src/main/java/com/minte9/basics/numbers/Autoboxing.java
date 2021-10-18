e8658b83b09f3258ba1687e1ed607080e6a81ab5
minte9 <catalinprescure@gmail.com>
2021-10-18 21:08:15 +0300

    java-pages update

--- src/main/java/com/minte9/basics/numbers/Autoboxing.java
+++ src/main/java/com/minte9/basics/numbers/Autoboxing.java
@@ -1,6 +1,9 @@
 /**
  * Primitives types cannot be used in Collections or Generics.	
  * Java compiler makes automatic convertions to primitives. 
+ * 
+ * Unboxing is the automatic conversion of objects into their ...
+ * coresponding primitives
  */
 
 package com.minte9.basics.numbers;
@@ -13,12 +16,15 @@ public class Autoboxing {
         
         List<Integer> li = new ArrayList<>();
 
-        for (int i=0; i<10; i++) { 
-            li.add(i);
-                // i is primitive
+        for (int i=10; i<20; i++) { 
+            li.add(i); // autoboxing (i is primitive)
         }
 
         System.out.println(li.get(0).getClass().getName());
             // java.lang.Integer
+
+        int b = li.get(0); // unboxing (element is Integer)
+        
+        System.out.println(b); // 10
     }
 }
