c2fa0185af7453c38e58424254699d39ba98eada
minte9 <catalinprescure@gmail.com>
2021-10-19 16:28:18 +0300

    java-pages update

--- src/main/java/com/minte9/basics/regexp/Split.java
+++ src/main/java/com/minte9/basics/regexp/Split.java
@@ -10,9 +10,10 @@ import java.util.regex.Pattern;
 public class Split {
     public static void main(String[] args) {
 
+        // split by char
         Pattern p = Pattern.compile("[ ,.!]");
         String ss[] = p.split("one two,alpha9 12!done.");
-        
+                
         for (String s : ss) {
             System.out.println(s);
             /*
@@ -23,6 +24,15 @@ public class Split {
                 done
             */
         }
+
+        // split by camel case
+        String s = "AbCdEf";
+        String[] words = s.split("(?=[A-Z])"); // Look Ahead
+        for(String word:words) {
+            System.out.println(word);
+                // Ab
+                // Cd
+                // Ef
+        }
     }
 }
-
