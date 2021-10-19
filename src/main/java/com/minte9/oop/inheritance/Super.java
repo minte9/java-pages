6927ea32d6ba433e721df76f5e6a12f7eaf00166
minte9 <catalinprescure@gmail.com>
2021-10-19 21:33:56 +0300

    java-pages update

---
+++ src/main/java/com/minte9/oop/inheritance/Super.java
@@ -0,0 +1,33 @@
+/**
+ * super() method is used to call parent constructor
+ * super() method must be the first statement in each controller
+ */
+
+package com.minte9.oop.inheritance;
+
+public class Super {
+    public static void main(String[] args) {
+        new A();
+    }
+}
+
+class A extends ParentClass {
+    public A() {
+        super(); // Parent constructor
+    }
+
+    /*
+    public void A(String s) {
+        super(s);
+        
+        // Error: Call to super must be the first statement
+        // void is first statement
+    }
+    */
+}
+
+class ParentClass {
+    public ParentClass() {
+        System.out.println("Parent constructor");
+    }
+}
