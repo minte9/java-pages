23480b38909e9610eca0f9106b9c2e4ed6d04c49
minte9 <catalinprescure@gmail.com>
2021-10-19 11:07:49 +0300

    java-pages update

---
+++ src/main/java/com/minte9/basics/strings/Converting.java
@@ -0,0 +1,27 @@
+/**
+ * To convert String to Number use valueOf()
+ * To convert Number to String use toString()
+ */
+
+package com.minte9.basics.strings;
+
+public class Converting {
+    public static void main(String[] args) {
+        
+        String a1 = "1";
+        String a2 = "1.11";
+        Integer b1 = Integer.valueOf(a1);
+        Float b2 = Float.valueOf(a2);
+
+        System.out.println(b1 + b2); // 2.1100001
+
+        int i = 11;
+        String a = "" + i;
+        String b = String.valueOf(i);
+        String c = Integer.toString(i);
+
+        System.out.println(a); // 11
+        System.out.println(b); // 11
+        System.out.println(c); // 11
+    }
+}
