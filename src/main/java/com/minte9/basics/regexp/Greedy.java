9e7a4d6f7558ad83d39f657b8aff81dcdcf2eea2
minte9 <catalinprescure@gmail.com>
2021-10-19 12:28:42 +0300

    java-pages update

--- src/main/java/com/minte9/basics/regexp/Greedy.java
+++ src/main/java/com/minte9/basics/regexp/Greedy.java
@@ -2,8 +2,10 @@
  * In the followingcode you might have expect two matches: 
  * "extend" and "end"
  * 
- * This is call "greedy behavior" ...
+ * This is call GREEDY behavior ...
  * which match the longest sequence.
+ * 
+ * Use "?" modifier for UNGREEDY behavior
  */
 
 package com.minte9.basics.regexp;
@@ -12,13 +14,23 @@ import java.util.regex.*;
 public class Greedy {
     public static void main(String[] args) {
 
-        Pattern p = Pattern.compile("e.+d"); // Look Here
+        // greedy
+        Pattern p = Pattern.compile("e.+d");
         Matcher m = p.matcher("extend cup end table");
 
         while(m.find()) {
-
             System.out.println(m.group());
                 // extend cup end
+        }
+
+        // ungreedy
+        Pattern p2 = Pattern.compile("e.+?d"); // Look Here
+        Matcher m2 = p2.matcher("extend cup end table");
+
+        while(m2.find()) {
+            System.out.println(m2.group());
+                // extend
+                // end
         }
     }
 }
