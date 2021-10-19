7fae0da7b2e748b729a1a8fa33daf621fe813e95
minte9 <catalinprescure@gmail.com>
2021-10-19 10:17:51 +0300

    java-pages update

--- src/test/java/basics/PassedByValueTest.java
+++ src/test/java/basics/PassedByValueTest.java
@@ -6,20 +6,27 @@ import org.junit.Test;
 public class PassedByValueTest {
 
     @Test public void objectTest() {
+
         MyClass obj = new MyClass();
+
         int x = 3;
         obj.init(x);
 
-        assertEquals(3, x); // pass
-        assertFalse(x == 0); // pass
+        assertEquals(x, 3); 
+            // pass
+        assertFalse(x == 0); 
+            // pass
     }
 
     @Test public void staticTest() {
+
         String y = "aaa";
         MyClass.set(y);
 
-        assertEquals(MyClass.y, "zzz"); // pass
-        assertFalse(MyClass.y.equals("aaa")); // pass
+        assertEquals(MyClass.y, "zzz"); 
+            // pass
+        assertFalse(MyClass.y.equals("aaa")); 
+            // pass
     }
 }
 
