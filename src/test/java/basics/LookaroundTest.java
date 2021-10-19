7ae9d15916c02b8a633c340e657ddd67f22a1419
minte9 <catalinprescure@gmail.com>
2021-10-19 17:29:05 +0300

    java-pages update

---
+++ src/test/java/basics/LookaroundTest.java
@@ -0,0 +1,33 @@
+package basics;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+public class LookaroundTest {  
+
+    public static boolean find(String str, String regex) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        return m.find();
+    } 
+    
+    @Test public void lookBehind() {
+        assertTrue(find("axyz", "(?<=a)xyz")); // pass
+        assertFalse(find("axyz", "(?<=b)xyz")); // pass
+    }
+
+    @Test public void lookAhead() {
+        assertTrue(find("axyz", "(?=x)xyz")); // pass
+        assertFalse(find("axyz", "(?=x)axyz")); // pass
+    } 
+
+    @Test public void lookBehindNegative() {
+        assertFalse(find("axyz", "(!?<=a)xyz")); // pass
+    }
+
+    @Test public void lookAheadNegative() {
+        assertFalse(find("axyz", "(?!x)xyz")); // pass
+    }
+}
