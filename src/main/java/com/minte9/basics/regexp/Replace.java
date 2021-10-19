7d786d59e981f760ca75cff643c4064345ac8e72
minte9 <catalinprescure@gmail.com>
2021-10-19 12:40:16 +0300

    java-pages update

---
+++ src/main/java/com/minte9/basics/regexp/Replace.java
@@ -0,0 +1,22 @@
+/**
+ * Matcher replaceAll() replace the matches from pattern ...
+ * with the provided string.
+ */
+
+package com.minte9.basics.regexp;
+import java.util.regex.*;
+
+public class Replace {
+    public static void main(String[] args) {
+
+        Pattern p = Pattern.compile("abc.*? ");
+        Matcher m = p.matcher("abc abcd bcd cef");
+            // Match: abc, (zero or more), ungreedy, space
+
+        while(m.find()) {
+            String r = m.replaceAll("XXX "); // Look Here
+            System.out.println(r);
+                // XXX XXX bcd cef
+        }
+    }
+}
