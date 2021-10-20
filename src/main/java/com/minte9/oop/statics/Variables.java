6391ee1f000cfbe247d4a35b849175249fe575fe
minte9 <catalinprescure@gmail.com>
2021-10-20 17:44:34 +0300

    java-pages update

---
+++ src/main/java/com/minte9/oop/statics/Variables.java
@@ -0,0 +1,34 @@
+/**
+ * Static variables are initialized only once.
+ * 
+ * A static class variable is common to all objects created with that class. 
+ * It can be called without object instantiation.
+ */
+
+package com.minte9.oop.statics;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        A a1 = new A();
+        A a2 = new A();
+        
+        System.out.println(a1.getCount()); // 2
+        System.out.println(a2.getCount()); // 2
+
+        System.out.println(A.id); // 100
+    }
+}
+
+class A {
+
+    private static int count = 0;
+    public static int id = 100;
+
+    public A() {
+        count++;
+    }
+    public int getCount() {
+        return count;
+    }
+}
