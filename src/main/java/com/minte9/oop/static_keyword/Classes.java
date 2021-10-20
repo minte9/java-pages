9de5d298f791244344141cfa565276a37294c027
minte9 <catalinprescure@gmail.com>
2021-10-20 18:09:13 +0300

    java-pages update

---
+++ src/main/java/com/minte9/oop/static_keyword/Classes.java
@@ -0,0 +1,25 @@
+/**
+ * Only nested classes can be static.
+ * 
+ * You can use the nested class without an instance for outer class.
+ */
+
+package com.minte9.oop.static_keyword;
+
+public class Classes {
+    public static void main(String[] args) {
+        
+        A.InnerClass.run(); // 10
+    }
+
+    static class A {
+        static int a = 10;
+    
+        static class InnerClass {
+            public static void run() {
+                System.out.println(a); // field from Outer Class
+            }
+        }
+    }
+    
+}
