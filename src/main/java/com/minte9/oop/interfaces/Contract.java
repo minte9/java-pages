c686bd4ef2cd0a85d8c5765cd7168d28be32db06
minte9 <catalinprescure@gmail.com>
2021-10-21 22:12:30 +0300

    java-pages update

--- src/main/java/com/minte9/oop/interfaces/Contract.java
+++ src/main/java/com/minte9/oop/interfaces/Contract.java
@@ -25,11 +25,16 @@ class Dog implements Canine {
     }
 }
 
-interface Canine { // no class keyword
+interface Canine { 
+        // no class keyword
+    
     public abstract void play();
-    public void eat(); // abstract by default
 
-    // public void move() {}; // Error: Interface methods cannot have body
+    public void eat(); 
+        // abstract by default
+
+    // public void move() {}; 
+        // Error: Interface methods cannot have body
 }
 
 interface Feline {
