9cb66e245b03a5f71bcbd847378ff0ce5dc2cf66
minte9 <catalinprescure@gmail.com>
2021-10-21 21:02:47 +0300

    java-pages update

---
+++ src/main/java/com/minte9/oop/abstract_keyword/Instantiation.java
@@ -0,0 +1,30 @@
+/**
+ * By marking the class as abstract, you can prevent instantiation.
+ * 
+ * What exactly is an Animal object, what color, what size?
+ * We don't know, so the class must be defined as abstract.
+ */
+
+package com.minte9.oop.abstract_keyword;
+
+public class Instantiation {
+    public static void main(String[] args) {
+        
+        new Dog("gray");
+
+        //new Animal(); // force compile error - what color?
+    }
+}
+
+abstract class Animal {
+    protected String color;
+    public Animal(String c) {
+        color = c;
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String c) {
+        super(c);
+    }
+}
