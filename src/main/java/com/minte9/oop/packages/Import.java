ff351c4cee1e7a06f5059c64e4e03f704d1b54d6
minte9 <catalinprescure@gmail.com>
2021-10-22 15:01:09 +0300

    java-pages update

---
+++ src/main/java/com/minte9/oop/packages/Import.java
@@ -0,0 +1,23 @@
+/**
+ * If you do not use a package statement, your type goes in an unnamed package.
+ * 
+ * Java compiler automaticaly imports three packages:
+ * (1) the package with no name
+ * (2) the java.lang package
+ * (3) the current package
+ */
+
+package com.minte9.oop.packages;
+
+import java.util.ArrayList;
+
+public class Import {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        System.out.println(list); // [1, 2]
+    }
+}
