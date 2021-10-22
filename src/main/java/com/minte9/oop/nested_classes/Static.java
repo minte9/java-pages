f5a95bdb8d4db343d8b3a694096ebbaea90effec
minte9 <catalinprescure@gmail.com>
2021-10-22 13:21:12 +0300

    java-pages update

--- src/main/java/com/minte9/oop/nested_classes/Static.java
+++ src/main/java/com/minte9/oop/nested_classes/Static.java
@@ -1,5 +1,24 @@
+/**
+ * Only nested classes can be declared as static classes.
+ * Inner classes can use fields from outer class.
+ */
+
 package com.minte9.oop.nested_classes;
 
 public class Static {
-    
+    public static void main(String[] args) {
+        
+        A.B.run(); // 10
+    }
+
+    static class A {
+
+        static int a = 10;
+        static class B {
+            
+            public static void run() {
+                System.out.println(a); // field from Outer Class
+            }
+        }
+    }
 }
