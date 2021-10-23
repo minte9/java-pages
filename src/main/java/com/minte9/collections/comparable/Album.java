30b94231ab8445ba45c22c52424db6db7b7030eb
minte9 <catalinprescure@gmail.com>
2021-10-23 20:37:20 +0300

    java-pages update

---
+++ src/main/java/com/minte9/collections/comparable/Album.java
@@ -0,0 +1,41 @@
+/**
+ * When you are sorting objects, the compiler doesn't know what to sort.
+ * The object by we sort needs to implements Comparable.
+ */
+
+package com.minte9.collections.comparable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Album {    
+    public static void main(String[] args) {
+
+        ArrayList<Song> myList = new ArrayList<Song>();
+        myList.add(new Song("Song A"));
+        myList.add(new Song("Song B"));
+        myList.add(new Song("Song C"));
+        
+        Collections.sort(myList); 
+        System.out.println(myList); // [Song A, Song B, Song C]
+    
+    }
+}
+
+class Song implements Comparable<Song> {
+        
+    String title;
+    
+    @Override public int compareTo(Song s) {
+        return title.compareTo(s.title); // Look Here
+            // Uses overrided method toString()
+    }
+    
+    public Song(String t) {
+        title = t;
+    }
+    
+    @Override public String toString() { // Look Here
+        return title;
+    }
+}
