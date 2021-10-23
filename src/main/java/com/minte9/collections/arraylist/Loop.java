2a25e0df80e567122f5016a5e55b5e6b12fb5853
minte9 <catalinprescure@gmail.com>
2021-10-23 20:03:41 +0300

    java-pages update

---
+++ src/main/java/com/minte9/collections/arraylist/Loop.java
@@ -0,0 +1,36 @@
+/**
+ * ArrayList can be parsed easily with enhanced for.
+ * 
+ * ArrayDeque can add elements to the head as fast as to the tail.
+ * If you are modifing the tail only, the ArrayList is a preferred.
+ */
+
+package com.minte9.collections.arraylist;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class Loop {    
+    public static void main(String[] args) {
+        
+        ArrayList<String> myList = new ArrayList<String>();
+
+        String x = "a";
+        String y = "b";
+        String z = "c";
+
+        myList.add(x);
+        myList.add(y);
+        myList.add(0, z); // add to first position
+
+        for(String v:myList) {
+            System.out.println(v); // c a b
+        }
+
+        ArrayDeque<Integer> myDeque = new ArrayDeque<>();
+        myDeque.addFirst(1);
+        myDeque.add(2);
+        myDeque.addLast(10);
+        myDeque.forEach(System.out::println); // 1 2 10
+    }
+}
