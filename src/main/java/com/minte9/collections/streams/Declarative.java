a47e06803513a9b8e3acec3a1f0ebcf054df5506
minte9 <catalinprescure@gmail.com>
2021-10-24 19:17:49 +0300

    java-pages update

--- src/main/java/com/minte9/collections/streams/Declarative.java
+++ src/main/java/com/minte9/collections/streams/Declarative.java
@@ -16,32 +16,25 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Declarative {
+
+    static List<Integer> mySquareList = new ArrayList<>();
+    static List<Integer> mylist = Arrays.asList(
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    );
     
     public static void main(String[] args) {
 
-        List<Integer> mylist = Arrays.asList(
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-        );
-
-
-        /**
-         * Imperative programming style
-         */
-        List<Integer> mySquareList = new ArrayList<>();
+        // Imperative programming
         for(Integer n : mylist) {
             if (n % 2 == 0) {
                 mySquareList.add(n * n);
             }
         }
-        System.out.println(mySquareList); // [4, 16, 36, 64, 100]
+        System.out.println(mySquareList); 
+            // [4, 16, 36, 64, 100]
 
 
-        /**
-         * Declarative programming (streams without lambdas)
-         * 
-         * Might look more confusing, but it cleary stating
-         * what operation each part of the stream is doing
-         */
+        // Declarative - no lambdas
         mySquareList = mylist.stream()
             .filter(
                 new Predicate<Integer>() {
@@ -57,18 +50,16 @@ public class Declarative {
                 }
             )
             .collect(Collectors.toList());
-        System.out.println(mySquareList); // [4, 16, 36, 64, 100]
+        System.out.println(mySquareList); 
+            // [4, 16, 36, 64, 100]
 
 
-        /**
-         * Declarative programming (lambdas)
-         * 
-         * Simple and reusable
-         */
+        // Declarative - lambdas
         mySquareList = mylist.stream()
             .filter(x -> x % 2 == 0)
             .map(y -> y * y)
             .collect(Collectors.toList());
-        System.out.println(mySquareList); // [4, 16, 36, 64, 100]
+        System.out.println(mySquareList); 
+            // [4, 16, 36, 64, 100]
     }
 }
