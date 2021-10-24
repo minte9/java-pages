073bcbecd60994c5e1d65ff3f1ecf691603f536d
minte9 <catalinprescure@gmail.com>
2021-10-24 21:35:48 +0300

    java-pages update

--- src/main/java/com/minte9/collections/json/Json.java
+++ src/main/java/com/minte9/collections/json/Json.java
@@ -1,3 +1,14 @@
+/**
+ * Json is the standard format for the ...
+ * communiation between Web Services.
+ * 
+ * Even Json was derived from JavaScript...
+ * it is platform independent format.
+ * 
+ * One of the main advantages, compared to XML ...
+ * is the size of document. 
+ */
+
 package com.minte9.collections.json;
 
 import org.json.JSONObject;
@@ -6,7 +17,6 @@ public class Json {
     public static void main(String[] args) {
         
         String s = "{message:{type:feed,timestamp:1},error:0}";
-       
         JSONObject obj = new JSONObject(s);
 
         System.out.println(
