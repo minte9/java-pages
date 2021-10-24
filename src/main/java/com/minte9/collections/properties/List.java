f22fb76499576ac6efafdaecf1e0ba5b7ec1dc57
minte9 <catalinprescure@gmail.com>
2021-10-24 17:56:33 +0300

    java-pages update

--- src/main/java/com/minte9/collections/properties/List.java
+++ src/main/java/com/minte9/collections/properties/List.java
@@ -15,10 +15,9 @@ package com.minte9.collections.properties;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.Properties;
 
-public class Loop {
+public class List {
     public static void main(String[] args) {
 
         try {
@@ -30,20 +29,14 @@ public class Loop {
             );
             Properties props = new Properties();
             props.load(input);
-
-            Enumeration<?> e = props.propertyNames();
-            while(e.hasMoreElements()) {
-                String key = (String) e.nextElement();
-                System.out.println(
-                    key + " : " + props.getProperty(key)
-                );
-            }
+            props.list(System.out);
 
             /*
-                db.port : 9000
-                db.user : my_user
-                db.url : localhost
-                db.pass : my_password
+                -- listing properties --
+                db.user=my_user
+                db.pass=my_password
+                db.port=9000
+                db.url=localhost
             */
 
         } catch (IOException ex) {
