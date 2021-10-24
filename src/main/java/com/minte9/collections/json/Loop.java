f3387bdb544de6b494942f8db784efe2d2e20e10
minte9 <catalinprescure@gmail.com>
2021-10-24 22:09:24 +0300

    java-pages update

---
+++ src/main/java/com/minte9/collections/json/Loop.java
@@ -0,0 +1,36 @@
+/**
+ * Use getJSONArray() in order to loop an json
+ */
+
+package com.minte9.collections.json;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        String str = "{"
+            + "orders: ["
+                + "{sym:goog,amount:100,error:1},"
+                + "{sym:msft,amount:200,error:0}"
+            + "], error:0" +
+        "}";
+
+        JSONObject obj = new JSONObject(str);
+        JSONArray orders = obj.getJSONArray("orders");
+            //System.out.println(orders);
+
+        for(Object item : orders) {
+
+            JSONObject order = (JSONObject) item; // Look Here
+            System.out.println(""
+                + order.getString("sym") 
+                + ":"
+                + order.getInt("error")
+            );
+                // goog:1
+                // msft:0
+        }
+    }
+}
