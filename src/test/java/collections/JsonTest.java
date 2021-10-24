446500d41c9042c09d56feb7c2629419b5a7cef1
minte9 <catalinprescure@gmail.com>
2021-10-24 21:55:54 +0300

    java-pages update

---
+++ src/test/java/collections/JsonTest.java
@@ -0,0 +1,19 @@
+package collections;
+import static org.junit.Assert.assertEquals;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class JsonTest {
+
+    @Test public void json() {
+
+        String s = "{message:{type:feed,timestamp:1},error:0}";
+        JSONObject obj = new JSONObject(s);
+
+        assertEquals("feed", obj.getJSONObject("message").getString("type"));
+            // passed
+        assertEquals(0, obj.getInt("error"));
+            // passed
+    }
+}
