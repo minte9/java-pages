129317c9b73e09910a9589ad3c63e48ad6fd4eb0
minte9 <catalinprescure@gmail.com>
2021-10-24 17:50:19 +0300

    java-pages update

---
+++ src/main/java/com/minte9/collections/properties/Loop.java
@@ -0,0 +1,41 @@
+package com.minte9.collections.properties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        try {
+
+            InputStream input = new FileInputStream(
+                "java-pages/src/main/java/com/minte9/" + 
+                "collections/properties/" + 
+                "config.properties"
+            );
+            Properties props = new Properties();
+            props.load(input);
+
+            Enumeration<?> e = props.propertyNames();
+            while(e.hasMoreElements()) {
+                String key = (String) e.nextElement();
+                System.out.println(
+                    key + " : " + props.getProperty(key)
+                );
+            }
+
+            /*
+                db.port : 9000
+                db.user : my_user
+                db.url : localhost
+                db.pass : my_password
+            */
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
