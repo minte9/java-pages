fb811ba0a7d26982bd558b8060a9a13455fdf769
minte9 <catalinprescure@gmail.com>
2021-10-25 12:41:18 +0300

    java-pages update

---
+++ src/main/java/com/minte9/threads/creation/Extending.java
@@ -0,0 +1,45 @@
+/**
+ * Threads are not separate processes on the OS (user feels is like separate processes).
+ * A Java application starts up a main thread, main() on the bottom of the stack.
+ * A new thread creats a separate call stack, run() at the botttom of the stack.
+ * 
+ * Threads can be created by using two mechanisms.
+ * - extending Thread class
+ * - implementing Runnable
+ */
+
+package com.minte9.threads.creation;
+
+public class Extending extends Thread { // Look Here
+    public static void main(String[] args) {
+  
+        Thread t1 = new Extending();
+        Thread t2 = new Extending();
+        Thread t3 = new Extending();
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    public void run() {
+        try {
+            String name = Thread.currentThread().getName();
+
+            System.out.println(name);
+            Thread.sleep(1000);
+
+            System.out.println(name);
+            Thread.sleep(1000);
+
+        } catch (InterruptedException ex) {}
+    }
+}
+
+/* 
+    Thread-2 
+    Thread-1
+    Thread-0
+    Thread-2
+    Thread-0
+    Thread-1
+*/
---
+++ src/main/java/com/minte9/threads/creation/Implementing.java
@@ -0,0 +1,56 @@
+/**
+ * Thread implementing Runnable.
+ * Using lambdas the code is more concise and easy to read.
+ */
+
+package com.minte9.threads.creation;
+
+public class Implementing {    
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(
+            Thread.currentThread().getName()
+        ); // main
+
+
+        new Thread(
+            new MyClass()
+        ).start(); // Tread-0
+
+
+        new Thread(new Runnable() {
+            public void run() {
+                System.out.println(
+                    Thread.currentThread().getName()
+                );
+            }
+        }).start(); // Thread-1
+
+
+        new Thread(() -> { // Lambdas
+            System.out.println(
+                Thread.currentThread().getName()
+            );
+        }).start(); // Thread-2
+
+
+        System.out.println("Back in Main");
+    }
+}
+
+class MyClass implements Runnable {
+
+    @Override public void run() {
+        System.out.println(
+            Thread.currentThread().getName()
+        );
+    }
+}
+
+/* 
+    main
+    Thread-0
+    Thread-1
+    Back in Main
+    Thread-2
+*/
