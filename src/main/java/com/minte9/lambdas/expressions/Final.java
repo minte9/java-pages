8f1cc1ab335febb2801bbef52837bac600930b62
minte9 <catalinprescure@gmail.com>
2021-10-25 21:23:42 +0300

    java-pages update

---
+++ src/main/java/com/minte9/lambdas/expressions/Final.java
@@ -0,0 +1,30 @@
+/**
+ * To use a variable from outside an anonymous inner class the variable ...
+ * must be final (or effective final).
+ * 
+ * The Holder variable is effective final,
+ * even if the final keyword it is not required (Java 8)
+ */
+
+package com.minte9.lambdas.expressions;
+import java.util.Arrays;
+import javax.xml.ws.Holder;
+
+public class Final {
+    public static void main(String[] args) {
+
+        Holder<Integer> holder = new Holder<>(0);
+        Arrays.asList("a", "b", "c").forEach((x) -> {
+            holder.value ++;
+        });
+        System.out.println(holder.value); 
+            // 3
+        
+        final String str = "A";
+        Arrays.asList("a", "b", "c").forEach((x) -> {
+            // str = str + x; // Error: final local variable cannot be assigned
+        }); 
+        System.out.println(str);
+            // A
+    }
+}
