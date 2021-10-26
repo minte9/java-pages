186ca9915accb53d023bdd2deae0d75da782c384
minte9 <catalinprescure@gmail.com>
2021-10-26 17:08:35 +0300

    java-pages update

---
+++ src/main/java/com/minte9/lambdas/default_methods/Functional.java
@@ -0,0 +1,36 @@
+/**
+ * Default methods can be used in lambda expresions.
+ * Functional interface must contain only one abstract method.
+ */
+
+package com.minte9.lambdas.default_methods;
+import java.util.Arrays;
+import java.util.List;
+
+public class Functional {
+  public static void main(String[] args) {
+      
+    List<MyValue> myList = Arrays.asList(
+      () -> 1, 
+      () -> 3,
+      () -> 2 // lambda: get()
+    );
+
+    int maxDoubled = myList.stream()
+      .mapToInt(x -> x.getDouble()) // lambda: default method
+      .max()
+      .orElse(0)
+    ;
+
+    System.out.println(maxDoubled); // 6
+  }
+}
+
+@FunctionalInterface interface MyValue {
+
+  int get(); // abstract
+  
+  default int getDouble() {
+    return get() * 2;
+  }
+}
