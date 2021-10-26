3c6b5bd41ac2f8c01eb76107c54c7f489c3ac53b
minte9 <catalinprescure@gmail.com>
2021-10-26 17:32:26 +0300

    java-pages update

---
+++ src/main/java/com/minte9/lambdas/default_methods/Object.java
@@ -0,0 +1,30 @@
+/**
+ * Default methods can be accessed only with object references! 
+ */
+
+package com.minte9.lambdas.default_methods;
+import static org.junit.Assert.assertEquals;
+
+public class Object {
+  public static void main(String[] args) {
+      
+    Formula a = x -> x * 2; // lambda
+    assertEquals(4, a.calculate(2)); 
+      // pass
+
+    Formula b = x -> a.sqrt(x); // Look Here
+    assertEquals(4, b.calculate(16)); 
+      // pass
+
+    //Formula c = x -> sqrt(x); // Not working
+  }
+}
+
+@FunctionalInterface interface Formula {
+
+  abstract int calculate(int x);
+
+  default int sqrt(int x) {
+    return (int) Math.sqrt(x);
+  }
+}
