ac1632d49a3d8232e7927ca2a7e52acd90060bd5
minte9 <catalinprescure@gmail.com>
2021-10-26 13:24:12 +0300

    java-pages update

--- src/main/java/com/minte9/lambdas/streams/Iterations.java
+++ src/main/java/com/minte9/lambdas/streams/Iterations.java
@@ -15,33 +15,24 @@ public class Iterations {
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3);
-        int count;
+        int count = 0;
 
-        count = 0;
         for(int n : numbers) {
-            if (n <= 2) {
-                count++;
-            }
+            if (n <= 2) count++;
         }
-        System.out.println(count); 
-            // 2
+        System.out.println(count); // 2
 
-        count = 0;
         Iterator<Integer> it = numbers.iterator();
         while(it.hasNext()) {
             int n = it.next();
-            if (n <= 2) {
-                count++;
-            }
+            if (n <= 2) count++;
         }
-        System.out.println(count); 
-            // 2
+        System.out.println(count); // 4
 
         long total = 0;
         total = numbers.stream()
                 .filter(n -> n <= 2)
                 .count();
-        System.out.println(total); 
-            // 2
+        System.out.println(total); // 2
     }
 }
