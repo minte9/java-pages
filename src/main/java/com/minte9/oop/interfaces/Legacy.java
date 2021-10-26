62b99d1cdfceecd74d5f0ab29e268ab24c036992
minte9 <catalinprescure@gmail.com>
2021-10-26 16:54:15 +0300

    java-pages update

--- src/main/java/com/minte9/oop/interfaces/Legacy.java
+++ src/main/java/com/minte9/oop/interfaces/Legacy.java
@@ -11,7 +11,6 @@ public class Legacy {
     public static void main(String[] args) {
 
         new LA().setvalue(); // LA - setvalue
-        
         new LB().settype(); // LB - settype
     }
 }
