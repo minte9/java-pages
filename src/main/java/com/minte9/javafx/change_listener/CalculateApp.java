e8cf1f67ea9caebda3c384ee409fb98d46ef9c60
minte9 <catalinprescure@gmail.com>
2021-10-28 17:21:50 +0300

    java-pages update

---
+++ src/main/java/com/minte9/javafx/change_listener/CalculateApp.java
@@ -0,0 +1,61 @@
+/**
+ * CalculateApp class ...
+ * 
+ * with button action and slider listener
+ * 
+ * FXMLLoader calls initialize() to intialize the Controller
+ */
+
+package com.minte9.javafx.change_listener;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class CalculateApp {
+
+    private NumberFormat CURRENCY = NumberFormat.getCurrencyInstance();
+    private NumberFormat PERCENT = NumberFormat.getPercentInstance();
+    private BigDecimal tipPercentage = new BigDecimal(0.15);
+    
+    @FXML private TextField amountTextField;
+    @FXML private Label tipPercentageLabel;
+    @FXML private Slider tipPercentageSlider;
+    @FXML private TextField tipTextField;
+    @FXML private TextField totalTextField;
+
+    public static void main(String[] args) {
+        CalculateGui.main(args);
+    }
+
+    @FXML void calculateButtonPressed() {
+        try {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
+            tipTextField.setText(CURRENCY.format(tip));
+            totalTextField.setText(CURRENCY.format(total));
+        } catch (NumberFormatException ex) {
+            amountTextField.setText("Enter amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
+        }
+    }
+
+    @FXML void initialize() {
+        CURRENCY.setRoundingMode(RoundingMode.HALF_UP);
+        tipPercentageSlider.valueProperty().addListener( // Look Here
+            (o, v, newValue) -> {
+                tipPercentage = BigDecimal.valueOf(
+                    newValue.intValue() / 100.0
+                );
+                tipPercentageLabel.setText(PERCENT.format(tipPercentage));
+            }
+        );
+    }
+}
---
+++ src/main/java/com/minte9/javafx/change_listener/CalculateGui.java
@@ -0,0 +1,23 @@
+package com.minte9.javafx.change_listener;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalculateGui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override public void start(Stage stage) throws Exception {
+        Parent root = 
+            FXMLLoader.load(getClass().getResource("tipcalculator.fxml"));
+
+        stage.setTitle("Button Event FXML");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
