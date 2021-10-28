53d1db2993b13bac9a59c261dfca22d40f57fe3e
minte9 <catalinprescure@gmail.com>
2021-10-28 15:54:12 +0300

    java-pages update

---
+++ src/main/java/com/minte9/javafx/on_action/ActionApp.java
@@ -0,0 +1,13 @@
+package com.minte9.javafx.on_action;
+
+import javafx.fxml.FXML;
+
+public class ActionApp {
+    public static void main(String[] args) {
+        ActionAppGui.main(args);
+    }
+
+    @FXML void buttonPressed() { // Look Here
+        System.out.println("button pressed");
+    }
+}
---
+++ src/main/java/com/minte9/javafx/on_action/ActionAppGui.java
@@ -0,0 +1,22 @@
+package com.minte9.javafx.on_action;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ActionAppGui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("button.fxml"));
+
+        stage.setTitle("Button Event FXML");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
