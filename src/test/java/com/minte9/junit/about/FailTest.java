dbb419f0eedae66f79d7de5dace5a362d3d9f5f4
minte9 <catalinprescure@gmail.com>
2021-10-29 19:15:56 +0300

    java-pages update

--- src/test/java/com/minte9/junit/about/FailTest.java
+++ src/test/java/com/minte9/junit/about/FailTest.java
@@ -4,7 +4,7 @@
  * You might need to add hamcrest to Referenced Libraries.
 */
 
-package com.minte9.junit;
+package com.minte9.junit.about;
 import org.junit.Test;
 import static org.junit.Assert.fail;
 
