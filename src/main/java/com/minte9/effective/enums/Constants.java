f921c2615d35bb61ca22bd8ee93c7ebee7096a81
minte9 <catalinprescure@gmail.com>
2021-11-02 14:39:43 +0200

    java-pages update

---
+++ src/main/java/com/minte9/effective/enums/Constants.java
@@ -0,0 +1,31 @@
+/**
+ * Before Enum, a commont pattern was to use group contants
+ */
+
+package com.minte9.effective.enums;
+
+public class Constants {
+    public static void main(String[] args) {
+
+        System.out.println(
+            Utils.Color.ANSI_RED + "This text is red!" + 
+            Utils.Color.ANSI_RESET
+        );
+    }
+}
+
+class Utils {
+
+    public static class Color {
+
+        public static final String ANSI_RESET = "\u001B[0m";
+        public static final String ANSI_BLACK = "\u001B[30m";
+        public static final String ANSI_RED = "\u001B[31m";
+        public static final String ANSI_GREEN = "\u001B[32m";
+        public static final String ANSI_YELLOW = "\u001B[33m";
+        public static final String ANSI_BLUE = "\u001B[34m";
+        public static final String ANSI_PURPLE = "\u001B[35m";
+        public static final String ANSI_CYAN = "\u001B[36m";
+        public static final String ANSI_WHITE = "\u001B[37m";
+    }
+}
