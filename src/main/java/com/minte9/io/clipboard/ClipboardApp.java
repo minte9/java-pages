7587a93bd541e461318533a4e7a12d05cdb57fe4
minte9 <catalinprescure@gmail.com>
2021-12-06 16:50:00 +0200

    java-pages update

---
+++ src/main/java/com/minte9/io/clipboard/ClipboardApp.java
@@ -0,0 +1,50 @@
+/**
+ * Clipboard app:
+ * Use Ctrl-C to copy text in clipboard
+ * The text will be display real time on console.
+ */
+package com.minte9.io.clipboard;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+public class ClipboardApp {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        StringSelection ss = new StringSelection("abc");
+        Clipboard cp = Toolkit.getDefaultToolkit().getSystemClipboard();
+        cp.setContents(ss, null);
+
+        while(true) {
+            String text = getFromClipboard();
+            System.out.println(text);
+            Thread.sleep(2000);
+        }
+    }
+
+    public static void copyToClipboard(String text) {
+        Toolkit.getDefaultToolkit().getSystemClipboard()
+            .setContents(new StringSelection(text), null);
+    }
+
+    public static String getFromClipboard() {
+
+        Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard()
+            .getContents(null);
+
+        try {
+            if (t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                String text = t.getTransferData(DataFlavor.stringFlavor).toString();
+                return text;
+            }
+        } catch (UnsupportedFlavorException | IOException ex) {}
+        
+        return null;
+    }
+}
