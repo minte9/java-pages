3ee60b9086a02fab9dab567e3e5769edd3f24527
minte9 <catalinprescure@gmail.com>
2021-12-06 17:43:03 +0200

    java-pages update

--- src/main/java/com/minte9/io/clipboard/BulletsApp.java
+++ src/main/java/com/minte9/io/clipboard/BulletsApp.java
@@ -1,49 +1,60 @@
 /**
- * Clipboard app:
+ * Add bullets:
+ * Get text from clipboard and add bullets to every line.
  * Use Ctrl-C to copy text in clipboard
  * The clipboard text will be displayed on runtime.
  */
 package com.minte9.io.clipboard;
 
 import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.util.Arrays.asList;
 
-public class ClipboardApp {
+public class BulletsApp {
 
     public static void main(String[] args) throws InterruptedException {
 
-        StringSelection ss = new StringSelection("abc");
-        Clipboard cp = Toolkit.getDefaultToolkit().getSystemClipboard();
-        cp.setContents(ss, null);
+        CP.copyToClipboard(" abc \n def");
 
-        while(true) {
-            String text = getFromClipboard();
+        List<String> A;
+        String text;
+
+        while (true) {
+            text = CP.getFromClipboard();
+
+            A = asList(text.split("\n"));
+            text = A.stream()
+                    .map(x -> "* " + x)
+                    .collect(Collectors.joining("\n"));
             System.out.println(text);
             Thread.sleep(2000);
-        }       
+        }
     }
+}
 
+class CP {
     public static void copyToClipboard(String text) {
         Toolkit.getDefaultToolkit().getSystemClipboard()
-            .setContents(new StringSelection(text), null);
+                .setContents(new StringSelection(text), null);
     }
 
     public static String getFromClipboard() {
 
-        Transferable t = 
-            Toolkit.getDefaultToolkit().getSystemClipboard()
+        Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard()
                 .getContents(null);
 
         try {
             if (t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
                 return t.getTransferData(DataFlavor.stringFlavor).toString();
             }
-        } catch (UnsupportedFlavorException | IOException ex) {}
+        } catch (UnsupportedFlavorException | IOException ex) {
+        }
 
         return null;
     }
