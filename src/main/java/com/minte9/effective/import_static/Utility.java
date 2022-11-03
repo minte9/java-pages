332f197632fbbe02e4e2a0bd761fc54b2c6aae36
minte9 <catalinprescure@gmail.com>
2022-11-03 11:17:32 +0200

    java-pages update

--- src/main/java/com/minte9/effective/import_static/Utility.java
+++ src/main/java/com/minte9/effective/import_static/Utility.java
@@ -8,16 +8,15 @@
 package com.minte9.effective.import_static;
 
 import static java.lang.Math.PI; // Look Here 
-// import static java.lang.Math.E;
 
 public class Utility {
 
     public static void main(String[] args) {
 
         System.out.println(PI); // Look Here
-            // 3.141592653589793
+            // 3.14...
         
         System.out.println(java.lang.Math.E); 
-            // 2.718281828459045
+            // 2.71...
     }
 }
