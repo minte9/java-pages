0b011dc27ca040df1151e0d30bd9e7a6595b4c7b
minte9 <catalinprescure@gmail.com>
2022-11-23 15:24:19 +0200

    java-pages update

--- src/main/java/com/minte9/oop/static_keyword/Variables.java
+++ src/main/java/com/minte9/oop/static_keyword/Variables.java
@@ -13,21 +13,19 @@ public class Variables {
         A a1 = new A();
         A a2 = new A();
         
-        System.out.println(a1.getCount()); // 2
+        System.out.println(a1.getCount()); // 2 (not 1)
         System.out.println(a2.getCount()); // 2
-
-        System.out.println(A.id); // 100
     }
 }
 
 class A {
 
-    private static int count = 0;
-    public static int id = 100;
+    private static int count;
 
     public A() {
         count++;
     }
+
     public int getCount() {
         return count;
     }
--- src/test/java/com/minte9/basics/variables/PassedByValueTest.java
+++ src/test/java/com/minte9/basics/variables/PassedByValueTest.java
@@ -1,30 +1,30 @@
 /**
- * Variables in Java are passed by value (a copy), 
+ * Variables in Java are passed by copy, 
  * not by reference
  */
 
 package com.minte9.basics.variables;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-class A {
-    int x;
+public class PassedByValueTest {
 
-    public void init(int z) {
-        z = 0;
+    @Test public void objectTest() {
+
+        A obj = new A();
+
+        obj.x = 3;
+        assertTrue(obj.x == 3); // pass
+        
+        obj.init();
+        assertTrue(obj.x != 3); // pass
     }
 }
 
-public class PassedByValueTest {
+class A {
 
-    @Test public void objTest() {
-
-        A a = new A();
-        //x = 3;
-        a.init(3);
-        int x = 1000;
-        assertEquals(x, 1000);     // pass
-        assertTrue(x != 0);     // pass
+    int x;
+    public void init() {
+        x = 0;
     }
 }
