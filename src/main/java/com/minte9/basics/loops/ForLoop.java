9a61e4ab83f58c20efe38329368966886ef1926c
minte9 <catalinprescure@gmail.com>
2022-11-25 15:34:43 +0200

    java-pages update

--- src/main/java/com/minte9/basics/loops/ForLoop.java
+++ src/main/java/com/minte9/basics/loops/ForLoop.java
@@ -1,9 +1,8 @@
-/**
- * Loops
- * 
+/** 
  * For Loop, when you know how many loops
  * Enhanced For Loop, index not needed
- * ForEach, loop over a Collection
+ * ForEach, loop over a Collection, ofthen used with ...
+ * lambdas or  method reference
  */
 package com.minte9.basics.loops;
 import java.util.Arrays;
@@ -12,22 +11,17 @@ import java.util.List;
 public class ForLoop {            
     public static void main(String[] args) {
         
-        // for loop
         for (int i=0; i<3; i++) {
             System.out.println(i); // 0 1 2
         }
 
-        // enhanced
         int[] nums = {10, 20, 30};
         for (int no : nums) {
             System.out.println(no); // 10 20 30
         }
 
-        // foreach
         List<Integer> data = Arrays.asList(100, 200, 300);
-        data.forEach(x -> System.out.println(x));  // lambda
-        data.forEach(System.out::println); // method reference
-            // 100 200 300
-            // 100 200 300
+        data.forEach(x -> System.out.println(x));  // 100 200 300
+        data.forEach(System.out::println); // 100 200 300
     }
 }
