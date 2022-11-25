930eed8c50d2d0d564e24b856f4a34585228fb83
minte9 <catalinprescure@gmail.com>
2022-11-25 15:30:07 +0200

    java-pages update

--- src/main/java/com/minte9/basics/loops/ForLoop.java
+++ src/main/java/com/minte9/basics/loops/ForLoop.java
@@ -1,25 +1,33 @@
 /**
- * For loop can be used when you know ...
- * how many times you want to loop.
+ * Loops
  * 
- * Those three expressions in the for loop are optional ... 
- * but this form is not recommended.
+ * For Loop, when you know how many loops
+ * Enhanced For Loop, index not needed
+ * ForEach, loop over a Collection
  */
 package com.minte9.basics.loops;
+import java.util.Arrays;
+import java.util.List;
 
 public class ForLoop {            
     public static void main(String[] args) {
         
-        for (int i=0; i<10; i++) {
-            System.out.println(i); 
-                // 0 1 2 .. 9 
+        // for loop
+        for (int i=0; i<3; i++) {
+            System.out.println(i); // 0 1 2
         }
 
-        int i=0; for ( ;; ) { // infinite loop
-            if (i >= 10) break;
-            System.out.println(i++); 
-                // 0 1 2 .. 9
+        // enhanced
+        int[] nums = {10, 20, 30};
+        for (int no : nums) {
+            System.out.println(no); // 10 20 30
         }
+
+        // foreach
+        List<Integer> data = Arrays.asList(100, 200, 300);
+        data.forEach(x -> System.out.println(x));  // lambda
+        data.forEach(System.out::println); // method reference
+            // 100 200 300
+            // 100 200 300
     }
 }
-
