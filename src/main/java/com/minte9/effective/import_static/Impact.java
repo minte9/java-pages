7191b9f9bb2f9f9cbca415679a253b11fc487161
minte9 <catalinprescure@gmail.com>
2022-11-28 11:36:02 +0200

    java-pages update

--- src/main/java/com/minte9/effective/import_static/Impact.java
+++ src/main/java/com/minte9/effective/import_static/Impact.java
@@ -1,22 +1,17 @@
 /**
- * Import static
+ * Import static, negative impact
  * 
- * Dumping potentialy large classes into your namespace ...
+ * Dumping potentialy large classes into your namespace, 
  * might have a negative impact on readabilty and testability
 */
 
 package com.minte9.effective.import_static;
-
 import static java.lang.Math.*; // Look Here
 
 public class Impact {
-
     public static void main(String[] args) {
 
-        System.out.println(PI); 
-            // 3.141592653589793
-        
-        System.out.println(E); 
-            // 2.718281828459045
+        System.out.println(PI); // 3.141592653589793
+        System.out.println(E);  // 2.718281828459045
     }
 }
--- src/main/java/com/minte9/effective/import_static/Utility.java
+++ src/main/java/com/minte9/effective/import_static/Utility.java
@@ -1,21 +1,21 @@
 /**
  * Import static
  * 
- * You can use import static to avoid ...
- * qualifying with the class name. 
+ * To avoid qualifying with the class name 
  */
 
 package com.minte9.effective.import_static;
-
 import static java.lang.Math.PI; // Look Here 
 
 public class Utility {
-
     public static void main(String[] args) {
 
-        System.out.println(PI); // Look Here
-            // 3.14...
-        System.out.println(java.lang.Math.E); 
-            // 2.71...
+        System.out.println(
+            PI // Look Here
+        ); // 3.14...
+
+        System.out.println(
+            java.lang.Math.E
+        ); // 2.71...
     }
 }
