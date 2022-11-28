8d244503875852d72824b11b998bb76f65ff707e
minte9 <catalinprescure@gmail.com>
2022-11-28 11:55:42 +0200

    java-pages update

--- src/main/java/com/minte9/effective/import_static/Utility.java
+++ src/main/java/com/minte9/effective/import_static/Utility.java
@@ -2,20 +2,27 @@
  * Import static
  * 
  * To avoid qualifying with the class name 
+ * Dumping potentialy large classes into your namespace, 
+ * might have a negative impact on readabilty and testability
  */
 
 package com.minte9.effective.import_static;
 import static java.lang.Math.PI; // Look Here 
+import static java.lang.Math.*;
 
 public class Utility {
     public static void main(String[] args) {
 
         System.out.println(
-            PI // Look Here
-        ); // 3.14...
+            PI // Math.PI =3.141592653589793
+        );
+        
+        System.out.println(
+            java.lang.Math.E // 2.718281828459045
+        );
 
         System.out.println(
-            java.lang.Math.E
-        ); // 2.71...
+            round(42.2) // Math.round(42.2)) = 42
+        );
     }
 }
