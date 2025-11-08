d1a5b2b7e1722114cb26fe3d71a44ef601f00c3b
minte9 <catalinprescure@gmail.com>
2025-11-08 12:43:14 +0200

    java-pages update

--- src/main/java/com/minte9/basics/classes/Classes.java
+++ src/main/java/com/minte9/basics/classes/Classes.java
@@ -1,9 +1,12 @@
 /**
- * In Java, everything goes into a class (.java file)
- * JVM compiles the file, a .class file is created
+ * In Java, everything goes into a class
+ * src/main/Classes.java
  * 
- * The program starts in main()
- * The return type void means there is no returned value
+ * JVM (java virtual machine) compiles the class and the archive is created
+ * target/classes/Class.class
+ * 
+ * Every java program starts starts in main()
+ * Return type void means there is no returned value
  * Every statement must end in semicolon
  */
 
@@ -12,6 +15,6 @@ package com.minte9.basics.classes;
 public class Classes {
     public static void main( String[] args ) {
 
-        System.out.println("Hello World!"); // Hello World!
+        System.out.println("Hello World!");  // Hello World!
     }
 }
