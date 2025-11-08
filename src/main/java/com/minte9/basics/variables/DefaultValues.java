c9000fd80243b4af1f576b0ad52926e5627e00dc
minte9 <catalinprescure@gmail.com>
2025-11-08 19:10:49 +0200

    java-pages update

--- src/main/java/com/minte9/basics/variables/DefaultValues.java
+++ src/main/java/com/minte9/basics/variables/DefaultValues.java
@@ -1,6 +1,9 @@
 /**
  * Class instance variables always have default value.
  * Local variable does not have a default value, they must initialized.
+ * 
+ * Local variables must be initialized before use.
+ * Null is allowed anywhere.
  */
 package com.minte9.basics.variables;
 
@@ -11,21 +14,31 @@ public class DefaultValues {
 }
 
 class Values {
-    int a;      // 0
-    float b;    // 0.0
+    int a;  // 0
+    float b;  // 0.0
     boolean c;  // false
-    Values v;   // null
+    MyObj v;  // null
 
     public void showValues() {
-        String local_variable = "a"; // no default value
+        String local_variable = "a";  // no default value
 
         System.out.println("int a = " + a);
         System.out.println("float b = " + b);
         System.out.println("boolean c = " + c);
         System.out.println("object v = " + v);
         System.out.println("String local_var = " + local_variable);
+
+        /**
+            int a = 0
+            float b = 0.0
+            boolean c = false
+            object v = null
+            String local_var = a
+         */
     }
 }
+
+class MyObj {}
 
 /*
     int a = 0
