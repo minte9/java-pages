406ea28ec46d327d98256e4538b4568f7afc2f01
minte9 <catalinprescure@gmail.com>
2025-11-08 19:30:12 +0200

    java-pages update

--- src/main/java/com/minte9/basics/variables/PassedByValue.java
+++ src/main/java/com/minte9/basics/variables/PassedByValue.java
@@ -2,21 +2,28 @@
  * Variables in Java are passed by value (copy), not by reference
  * 
  * In this example, first variable x = 7 bits are copied (00000111).
- * Second this copy goes in z variable.
- * Third, after variable z changes, we can see that x is not changed.
+ * Then this copy goes in z variable (z = x)
+ * Then after variable z changes (z = 0), x is not changed.
  */
 
 package com.minte9.basics.variables;
+
 public class PassedByValue {
     public static void main(String[] args) {
 
         int x = 7; // 00000111
         int z = x; // x bits are copied in z
-        System.out.println("x == z " + (x == z));
 
-        z = 0;
-        System.out.println("z changed / x doesn't change");
-        System.out.println("x != z " + (x != z));
+        System.out.println("First: x == z " + (x == z));
+
+        z = 0;  // x is not changed
+
+        System.out.println("Second: x != z " + (x != z));
+
+        /**
+            First: x == z true
+            Second: x != z true
+         */
     }
 }
 
