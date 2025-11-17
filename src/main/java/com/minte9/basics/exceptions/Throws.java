e254fbb3fbb4a3cda3691717e12843fab5f152c3
minte9 <catalinprescure@gmail.com>
2025-11-17 16:10:15 +0200

    java-pages update

--- src/main/java/com/minte9/basics/exceptions/Throws.java
+++ src/main/java/com/minte9/basics/exceptions/Throws.java
@@ -10,21 +10,18 @@ public class Throws {
     public static void main(String[] args) {
 
         try {
-            A.check("wrong"); // the line is ok for compiler
+            check("wrong"); // Correct code (for compiler)
         } catch (Exception e) {
-            System.out.print(e.getMessage());  // Wrong value exception!
+            System.out.println(e.getMessage());  // Wrong value exception!
         }
     }
-    
-    
-}
 
-class A {
-    static boolean check(String s) throws Exception { // Look Here
+    private static boolean check(String s) throws Exception { // Look Here
         
         if (s.equals("wrong")) {
             throw new Exception("Wrong value exception!");
         }
         return true;
-    }
+    } 
+    
 }
