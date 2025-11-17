e138db81f56e64d7788e2692e89b7c61cb14ba19
minte9 <catalinprescure@gmail.com>
2025-11-17 15:06:05 +0200

    java-pages update

--- src/main/java/com/minte9/basics/regexp/Greedy.java
+++ src/main/java/com/minte9/basics/regexp/Greedy.java
@@ -1,6 +1,4 @@
 /**
- * Greedy, Ungreedy
- * 
  * Greedy behavior match the longest sequence
  * Use "?" modifier for ungreedy behavior
  */
@@ -15,22 +13,30 @@ public class Greedy {
         Pattern p;
         Matcher m;
 
-        p = Pattern.compile("e.+d"); // Greedy
-        m = p.matcher("extend cup end table");
+        String txt = "extend cup end table";
+
+        // Greedy
+        p = Pattern.compile("e.+d");
+        m = p.matcher(txt);
         while(m.find()) {
             System.out.println(m.group()); // extend cup end
         }
 
-        p = Pattern.compile("e.+?d"); // Ungreedy
-        m = p.matcher("extend cup end table");
+        // Ungreedy
+        p = Pattern.compile("e.+?d");
+        m = p.matcher(txt);
         while(m.find()) {
-            System.out.println(m.group()); // extend, end
+            System.out.println(m.group()); // extend
+                                           // end
         }
 
-        p = Pattern.compile("Java ?(8|SE)"); // Ungreedy
+
+        // Group example
+        p = Pattern.compile("Java ?(8|SE)");
         m = p.matcher("Java 8 Java SE");
         while(m.find()) {
-            System.out.println(m.group()); // Java 8 , Java SE
+            System.out.println(m.group()); // Java 8
+                                           // Java SE
         }
     }
 }
