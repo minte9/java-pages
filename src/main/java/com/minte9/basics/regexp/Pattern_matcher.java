f6f929df479af48be06904efcb8e65f9b2a10fb8
minte9 <catalinprescure@gmail.com>
2025-11-17 14:23:26 +0200

    java-pages update

--- src/main/java/com/minte9/basics/regexp/Pattern_matcher.java
+++ src/main/java/com/minte9/basics/regexp/Pattern_matcher.java
@@ -1,15 +1,13 @@
 /**
- * Pattern, Matcher
- * 
- * The Pattern matches() if all the string match the pattern
- * To check only parts of the string find() is used
+ * The Pattern matches() if all the string match the pattern.
+ * To check only parts of the string find() is used.
  */
 
 package com.minte9.basics.regexp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Matches {
+public class Pattern_matcher {
     public static void main(String[] args) {
 
         Pattern p = Pattern.compile("Java");
