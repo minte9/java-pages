0ff5ce21ca547bd11f5c870854adf9949a4d1329
minte9 <catalinprescure@gmail.com>
2025-11-21 19:46:48 +0200

    java-pages update

---
+++ src/main/java/com/minte9/oop/inheritance/examples/PaymentsSystem.java
@@ -0,0 +1,70 @@
+/**
+ * Inheritance real-world scenario: Payment systems
+ * Inheritance usage: Payment class defines common behavior, while subclasses implement specific logic.
+ * Extensibility: Adding new payment methods (e.g., CryptoPayment) is easy without changing existing code.
+ */
+
+package com.minte9.oop.inheritance.examples;
+
+public class PaymentsSystem {
+    public static void main(String[] args) {
+
+        Payment p1 = new CreditCardPayment("1234-5678-9012-3456");
+        p1.processPayment(150.00);
+
+        Payment p2 = new PayPalPayment("user@example.com");
+        p2.processPayment(75.50);
+    }
+}
+
+// Abstract superclass
+abstract class Payment {
+    public abstract void processPayment(double amount);
+    public abstract boolean validatePayment();
+}
+
+// Subclass
+class CreditCardPayment extends Payment {
+    private String cardNumber;
+
+    public CreditCardPayment(String s) {
+        this.cardNumber = s;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        if (!validatePayment()) {
+            System.out.println("CreditCard validation failed!");
+        } else {
+            System.out.println("CreditCard processing payment of $" + amount);  // processing OK
+        }
+    }
+
+    @Override
+    public boolean validatePayment() {
+        return cardNumber != null && cardNumber.length() == 19;  // simple validation
+    }
+}
+
+// Subclass
+class PayPalPayment extends Payment {
+    private String email;
+
+    public PayPalPayment(String s) {
+        this.email = s;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        if (!validatePayment()) {
+            System.out.println("PayPal validation failed!");
+        } else {
+            System.out.println("PayPall processing payment of $" + amount);  // processing OK
+        }
+    }
+
+    @Override
+    public boolean validatePayment() {
+        return email != null && email.contains("@"); // simple validation
+    }
+}
