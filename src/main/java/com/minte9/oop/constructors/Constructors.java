7cced2a79f4c1e3d6e57e93b0fc47430e6256ec6
minte9 <catalinprescure@gmail.com>
2025-11-22 14:43:16 +0200

    java-pages update

--- src/main/java/com/minte9/oop/constructors/Constructors.java
+++ src/main/java/com/minte9/oop/constructors/Constructors.java
@@ -8,38 +8,26 @@ package com.minte9.oop.constructors;
 public class Constructors {
     public static void main(String[] args) {
 
-        new A().test();
-        new B("aaa");
-        new B(100);
-
-        /*
-            Default constructor used
-            Constructor 1 - aaa
-            Constructor 2 - 100
-        */
+        new A();  // Default constructor used
+        new A("aaa");  // Constructor 1 - aaa
+        new A(100);  // Constructor 2 - 100
     }
 }
 
 class A {
-    
-    /*
-        public Output() {
-            // default constructor
-        }
-    */
 
-    public void test() {
+    // Default contructor (written explicitly for clarity)
+    public A() {
         System.out.println("Default constructor used");
     }
-}
 
-class B {
-    
-    public B(String s) {
+    // Constructor 1
+    public A(String s) {
         System.out.println("Constructor 1 - " + s);
     }
-    
-    public B(int i) {
-        System.out.println("Constructor 2 - " + i);
+
+    // Constructor 2
+    public A(int n) {
+        System.out.println("Constructor 2 - " + n);
     }
 }
