910835e8ba0bc9c5cb184264e16c64f06183f791
minte9 <catalinprescure@gmail.com>
2025-11-24 13:54:04 +0200

    java-pages update

--- src/main/java/com/minte9/collections/generics/Type.java
+++ src/main/java/com/minte9/collections/generics/Type.java
@@ -1,6 +1,27 @@
 /**
- * A generic type is a special kind of variable ...
- * with whatever type you pass in.
+ * Generics in Java
+ * 
+ * A generic type is a class or method that can operate on 
+ * different data types while still providing TYPE SAFETY.
+ * 
+ * Without generics, everything is threated as Object.
+ * This causes problems:
+ * You can store anything (String, Integer, etc).
+ * You must CAST whne retriving.
+ * 
+ * With generics (like MyClass<T>), Java enforces the type at 
+ * COMPILE TIME.
+ * 
+ * Generic were introduces in Java 5 to eliminate those 
+ * type-safe problems, especially when using collections.
+ * 
+ * What is T? 
+ * 
+ * T is a type placeholder.
+ * When you write Box<String> T becomes String.
+ * 
+ * Non generic classes (A) is legal, but unsafe.
+ * Java allowes it because generic where added later.
  */
 
 package com.minte9.collections.generics;
@@ -9,21 +30,28 @@ public class Type {
 
     public static void main(String[] args) {
         
-        A a = new A();
-        a.set(10);
-        System.out.println(a.get()); // 10
-        a.set("John");
-        System.out.println(a.get()); // John
+        // --- Non-generic class (unsafe) ----
+        NonGenericClass A = new NonGenericClass();
+        A.set(10);
+        System.out.println(A.get()); // 10
 
-        Box<Integer> b = new Box<>();
-        b.set(10);
-        System.out.println(b.get()); // 10
-        // b.set("John"); // Error: not applicable ...
+        A.set("John");  // allowed (but unsage)
+        System.out.println(A.get()); // John
+
+        // This compile but is dangerous:
+        //Integer x = (Integer) A.get(); // runtime crash
+
+        // --- Generic class (safe) ---
+        MyClass<Integer> B = new MyClass<>();
+        B.set(10);
+        System.out.println(B.get()); // 10
+
+        // B.set("John"); // ❌ compile-time error
+        // This is the whole point of generics.
     }
 }
 
-class A { // non generic class
-
+class NonGenericClass {
     private Object obj;
     public void set(Object o) { 
         obj = o;
@@ -33,8 +61,8 @@ class A { // non generic class
     }
 }
 
-class Box<T> { // generic class (type)
-        
+// Generic class
+class MyClass<T> {
     private T t;
     public void set(T t) { 
         this.t = t; 
