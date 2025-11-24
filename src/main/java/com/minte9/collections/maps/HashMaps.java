60558d770671e9893d9ae061948d8579fc0909f1
minte9 <catalinprescure@gmail.com>
2025-11-24 11:35:36 +0200

    java-pages update

--- src/main/java/com/minte9/collections/maps/HashMaps.java
+++ src/main/java/com/minte9/collections/maps/HashMaps.java
@@ -1,6 +1,20 @@
 /**
- * In a HashMap you can have duplicate values ...
- * but no duplicate keys.
+ * HashMap stores key-value pairs.
+ * 
+ * The keys must be UNIQUE, values may be DUPLICATES.
+ * If we put() a value using a key that alreasy exists,
+ * the old value is overwritten.
+ * 
+ * Internaly HashMap uses hashing to store the items in buckets.
+ *      - hashCode(key) to finds the bucket
+ *      - equals(key) to resolve collisions (same bucket)
+ * 
+ * Keys must implement proper equals() and hashCode()
+ * (String already does).
+ * 
+ * Performance:
+ *      - put() and get() are O(1) average time
+ *      - very fast for lookups by key
  */
 
 package com.minte9.collections.maps;
@@ -9,18 +23,14 @@ import java.util.HashMap;
 public class HashMaps {
     public static void main(String[] args) {
     
-        HashMap<String, Integer> scores = 
-            new HashMap<String, Integer>();
+        HashMap<String, Integer> scores = new HashMap<>();
         
-        scores.put("a", 1);
-        scores.put("a", 2); // overrites "a" key
-        scores.put("b", 2);
-        scores.put("c", 2);
+        scores.put("Mary", 10);
+        scores.put("Mary", 20); // overrites
+        scores.put("John", 20);
+        scores.put("Boby", 16);
 
-        System.out.println(scores); // {a=2, b=2, c=2}
-
-        System.out.println(
-            scores.get("a") // 2
-        );
+        System.out.println(scores); // {John=20, Boby=16, Mary=20}
+        System.out.println(scores.get("John"));  // 20
     }
 }
