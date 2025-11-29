0515490d1d23547b97d3e9e98ff8766cf06aedcc
minte9 <catalinprescure@gmail.com>
2025-11-29 13:26:19 +0200

    java-pages update

--- src/main/java/com/minte9/collections/streams/Streams.java
+++ src/main/java/com/minte9/collections/streams/Streams.java
@@ -1,39 +1,54 @@
 /**
- * With Java 8 Streams we can make bulk operations ...
- * sequentiallly or paralel.
+ * STREAM API (Java 8+)
+ * --------------------
+ * Streams allow functional-style bulk operations on data:
+ *      - filtering
+ *      - mappging
+ *      - sorting
+ *      - reducing (sum, average, min, max) 
  * 
- * The source for streams can be arrays, files, regex, etc.
- * We don't have to create a collection in order to work with streams.
+ * IMPORTANT:
+ * ---------
+ * A stream is NOT a collection.
+ * It does not store elements, it process them.
+ * 
+ * Streams can be created from:
+ *      - collections
+ *      - arrays (Arrays.stream())
+ *      - static stream buileder (Stream.of())
+ *      - files, regex, generators, etc
+ * 
+ * OPERATIONS:
+ * -----------
+ * A stream pipeline has:
+ *      1) Source
+ *      2) Intermediate operations (filter, map, sorted)
+ *      3) Terminal operation (forEach, collect, reduce)
+ * 
+ * Streams can run:
+ *      - sequentially (default)
+ *      - in paralel (parallelStream())
  */
 
 package com.minte9.collections.streams;
-import java.util.Arrays;
+
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
-        
-        List<String> myList = Arrays.asList("aa", "ba", "bc");
-        myList
-            .stream()
-            .filter(s -> s.startsWith("b"))
-            .map(String::toUpperCase)
-            .sorted()
-            .forEach(System.out::println);  
-                // BA BC
 
-        Stream.of("aa", "ba", "bc") // no collection
-            .filter(s -> s.startsWith("b"))
-            .map(String::toUpperCase)
-            .sorted()
-            .forEach(System.out::println); 
-                // BA BC    
+        List<Order> orders = List.of(
+            new Order("Laptop", 1200),
+            new Order("Mouse", 25),
+            new Order("Keyboard", 35)
+        );
 
-        Arrays.stream(new int[] {1, 2, 3}) // primitives
-            .map(n -> n * 2)
-            .average()
-            .ifPresent(System.out::println); 
-                // 4.0
+        double total = orders.stream()
+                .mapToDouble(Order::price)
+                .sum();
+            
+        System.out.println(total);  // 1260.0
     }
 }
+
+record Order(String name, double price) {}
