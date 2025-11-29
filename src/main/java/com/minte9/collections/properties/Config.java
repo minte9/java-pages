91123d1367823f1bae0d7ba67b4ce62bfc8ab5c1
minte9 <catalinprescure@gmail.com>
2025-11-29 10:57:53 +0200

    java-pages update

--- src/main/java/com/minte9/collections/properties/Config.java
+++ src/main/java/com/minte9/collections/properties/Config.java
@@ -1,11 +1,19 @@
 /**
- * Properties file to store project configuration data.
+ * Java properties file is normaly used to store project configuration 
+ * data or settings.
+
  * Use getProperty() to get values.
+ *
+ * Example: 
+ * main/resouces/config.properties:
+ *      db.url = localhost
+ *      db.user = my_user
+ *      db.pass = my_password
+ *      db.port = 9000
  */
 
 package com.minte9.collections.properties;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,24 +21,21 @@ import java.util.Properties;
 public class Config {
     public static void main(String[] args) throws IOException {
 
-        InputStream input = new FileInputStream(
-            "java-pages/src/main/java/com/minte9/" + 
-            "collections/properties/" + 
-            "config.properties"
-        );
+        InputStream input = Config.class
+            .getClassLoader()
+            .getResourceAsStream("config.properties");
+
+        if (input == null) {
+            throw new RuntimeException("config.properties not found");
+        }
+
         Properties props = new Properties();
         props.load(input);
 
         String user = props.getProperty("db.user");
-        int port = Integer.parseInt(
-            props.getProperty("db.port")
-        );
+        int port = Integer.parseInt(props.getProperty("db.port"));
 
-        System.out.println("user: " + user); // my_user
-        System.out.println("port: " + port); // 9000
+        System.out.println("user: " + user);  // my_user
+        System.out.println("port: " + port);  // 9000
     }
 }
-
-/*
-
-*/
