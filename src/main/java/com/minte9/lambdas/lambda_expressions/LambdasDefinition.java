a679086e97a0617e003e9e6d36da7d28825e73a3
minte9 <catalinprescure@gmail.com>
2025-12-14 12:42:44 +0200

    java-pages update

--- src/main/java/com/minte9/lambdas/lambda_expressions/LambdasDefinition.java
+++ src/main/java/com/minte9/lambdas/lambda_expressions/LambdasDefinition.java
@@ -1,5 +1,54 @@
+/**
+ * WHAT ARE LAMBDAS?
+ * -----------------
+ * A lambda expression is a concise way to represent implementations
+ * of functional interfaces.
+ * 
+ * Insteed of creating an object with an anonymous inner class,
+ * lambdas allow us to pass 'behavior' (executable code) directly.
+ * 
+ * FUNCTIONAL INTERFACES:
+ * ------------------------------
+ * A functional interface is an interface with one abstract method.
+ * Example:
+ *  - Runnable
+ *  - Comparator<T>
+ *  - Predicate<T>
+ *  - BinaryOperator<T>
+ * 
+ * COMMON LAMBDAS FORMS:
+ * ---------------------
+ * () -> expresion          // no parameters
+ * () -> { statement }      // multiple statements
+ * (x, y) -> x + y          // multiple parameters
+ */
+
 package com.minte9.lambdas.lambda_expressions;
 
+import java.util.function.BinaryOperator;
+
 public class LambdasDefinition {
-    
+    public static void main(String[] args) {
+
+        // -----------------------------------
+        // Lambda with no arguments (Runnable)
+        // -----------------------------------
+        Runnable noArgs = () -> System.out.println("Hello World");
+        new Thread(noArgs).run();
+        
+        // --------------------------------------
+        // Lambda with a full block of statements
+        // --------------------------------------
+        Runnable multiStatement = () -> {
+            System.out.println("Hello");
+            System.out.println("World");
+        };
+        new Thread(multiStatement).run();
+
+        // ---------------------------------------
+        // Lambda with parameters (BinaryOperator)
+        // ---------------------------------------
+        BinaryOperator<Integer> op = (x, y) -> x + y;
+        System.out.println(op.apply(2, 3));  // 5
+    }
 }
